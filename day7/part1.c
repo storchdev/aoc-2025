41d8d349374e038c38728fa33b858a0c22942c14
storchdev <pianister123@gmail.com>
2025-12-08 11:03:33 -0500

    day7

---
+++ day7/part1.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void beamsim(char *grid, int rows, int cols, int initr, int initc, int **v,
+             int *vlen, int *ans) {
+  for (int r = initr; r < rows; r++) {
+    if (grid[r * cols + initc] == '^') {
+      for (int i = 0; i < *vlen; i++) {
+        if (r * cols + initc == (*v)[i]) {
+          return;
+        }
+      }
+
+      (*v)[*vlen] = r * cols + initc;
+      (*vlen)++;
+
+      printf("Split at (%d,%d)\n", r, initc);
+      (*ans)++;
+      if (initc - 1 >= 0) {
+        beamsim(grid, rows, cols, r, initc - 1, v, vlen, ans);
+      }
+      if (initc + 1 < cols) {
+        beamsim(grid, rows, cols, r, initc + 1, v, vlen, ans);
+      }
+      break;
+    }
+  }
+}
+
+int main() {
+  char grid[100000];
+  int varr[100000];
+  int *v = (int *)varr;
+
+  int len = 0;
+  int rows = 0;
+  int cols = 0;
+
+  int x = '0';
+
+  int scol = 0;
+
+  while (x != EOF) {
+    x = getchar();
+
+    if (x == 'S') {
+      scol = len;
+    }
+    if (x == '\n') {
+      if (rows == 0) {
+        cols = len;
+      }
+      rows++;
+    } else {
+      grid[len] = x;
+      len++;
+    }
+  }
+
+  printf("%dx%d, len=%d, scol=%d\n", rows, cols, len, scol);
+  int ans = 0;
+
+  int vlen = 0;
+  beamsim(grid, rows, cols, 0, scol, &v, &vlen, &ans);
+
+  printf("Ans: %d\n", ans);
+  return 0;
+}
---
+++ day7/part2.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void beamsim(char *grid, int rows, int cols, int initr, int initc, long long w,
+             long long *ans) {
+  for (int r = initr; r < rows; r++) {
+    if (grid[r * cols + initc] == '^') {
+      // for (int i = 0; i < *vlen; i++) {
+      //   if (r * cols + initc == (*v)[i]) {
+      //     return;
+      //   }
+      // }
+      //
+      // (*v)[*vlen] = r * cols + initc;
+      // (*vlen)++;
+
+      // printf("Split at (%d,%d)\n", r, initc);
+      // (*ans)++;
+      if (initc - 1 >= 0) {
+        beamsim(grid, rows, cols, r, initc - 1, w * 2, ans);
+      }
+      if (initc + 1 < cols) {
+        beamsim(grid, rows, cols, r, initc + 1, w * 2, ans);
+      }
+      return;
+    }
+  }
+
+  (*ans) += w;
+}
+
+typedef struct {
+  int r;
+  int c;
+  long long v;
+} Pair;
+
+long long getValue(Pair *mem, int len, int r, int c) {
+  for (int i = 0; i < len; i++) {
+    Pair p = mem[i];
+    if (p.r == r && p.c == c) {
+      return p.v;
+    }
+  }
+  return -1;
+}
+
+typedef long long (*Dpfunc)(char *, int, int, int, int, Pair *, int *);
+
+long long getInc(char *grid, int rows, int cols, int r, int c, Pair *mem,
+                 int *memlen, Dpfunc dp) {
+  // either computes recursively or from memoized array
+
+  long long cached = getValue(mem, *memlen, r, c);
+  long long inc;
+  if (cached == -1) {
+    inc = dp(grid, rows, cols, r - 1, c, mem, memlen);
+    mem[*memlen] = (Pair){r, c, inc};
+    (*memlen)++;
+    printf("%lld more ways from (%d,%d)\n", inc, r, c);
+  } else {
+    inc = cached;
+  }
+  return inc;
+}
+
+// long long getInc() {}
+// answers "how many ways could a beam have gotten to (initr,initc)?"
+long long dp(char *grid, int rows, int cols, int initr, int initc, Pair *mem,
+             int *memlen) {
+  long long ret = 0;
+  for (int r = initr; r >= 0; r--) {
+    if (grid[r * cols + initc] == 'S') {
+      // 1 way from source
+      return 1;
+    }
+
+    if (r == 0)
+      continue;
+
+    // inductive cases
+    if (initc - 1 >= 0 && grid[r * cols + (initc - 1)] == '^') {
+      ret += getInc(grid, rows, cols, r, initc - 1, mem, memlen, dp);
+    }
+    if (initc + 1 < cols && grid[r * cols + (initc + 1)] == '^') {
+      ret += getInc(grid, rows, cols, r, initc + 1, mem, memlen, dp);
+    }
+
+    // beam cannot come from above ^
+    if (grid[r * cols + initc] == '^') {
+      break;
+    }
+  }
+  return ret;
+}
+
+int main() {
+  char grid[100000];
+  // int varr[100000];
+  // int *v = (int *)varr;
+
+  int len = 0;
+  int rows = 0;
+  int cols = 0;
+
+  int x = '0';
+
+  int scol = 0;
+
+  while (x != EOF) {
+    x = getchar();
+
+    if (x == 'S') {
+      scol = len;
+    }
+    if (x == '\n') {
+      if (rows == 0) {
+        cols = len;
+      }
+      rows++;
+    } else {
+      grid[len] = x;
+      len++;
+    }
+  }
+
+  printf("%dx%d, len=%d, scol=%d\n", rows, cols, len, scol);
+  long long ans = 0;
+
+  Pair mem[100000];
+
+  int memlen = 0;
+
+  for (int c = 0; c < cols; c++) {
+    ans += dp(grid, rows, cols, rows - 1, c, mem, &memlen);
+  }
+
+  printf("Ans: %lld\n", ans);
+  return 0;
+}
